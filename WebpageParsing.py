83b0aecca82fea992bdbeee272d216fd323d9da2
Achi Jones <jonesguy14@gmail.com>
2014-01-27 17:25:40 -0500

    fixed characters, theme title

--- WebpageParsing.py
+++ WebpageParsing.py
@@ -1,16 +1,22 @@
 from urllib.request import urlopen
 
-webpage = urlopen('http://www.sparknotes.com/lit/siddhartha/summary.html').read().decode('utf-8')
 #title
+webpage = urlopen('http://www.sparknotes.com/lit/siddhartha/summary.html').read().decode('utf-8')
 index = webpage.index("<title>")
 index2 = webpage.index("</title>")
-print(webpage[index+7:index2])
+print(webpage[index+7:index2].replace("SparkNotes: ","")+"\n")
+
 #characters
-characterpage = urlopen('http://www.sparknotes.com/lit/siddhartha/characters.html')#.read().decode('utf-8')
-print(characterpage.read().decode('utf-8'))
+print("Characters: ")
+characterpage = urlopen('http://www.sparknotes.com/lit/siddhartha/characters.html').read().decode()
 for line in characterpage.splitlines():
     if line.find("<b>")!=-1:
         start = line.index("<b>")
         end = line.index("</b")
-        print(webpage[start+3:end])
-        print("\n")
+        print(line[start+3:end].replace("&rsquo;","'")+", ")
+
+#themes
+themespage = urlopen('http://www.sparknotes.com/lit/siddhartha/themes.html').read().decode('utf-8')
+theme_tit_s = themespage.index("<title>")
+theme_tit_e = themespage.index("</title>")
+print(themespage[theme_tit_s+7:theme_tit_e].replace("SparkNotes: ","")+"\n")
